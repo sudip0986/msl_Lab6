8750fb2cc09c1e1d3ded4e250b4077bb71a9cf56
sudip0986 <iamsudipbasak1@gmail.com>
2026-09-24 18:12:00 +0600

    Add Lab 7 project

--- public/first.js
+++ public/first.js
@@ -1,1 +1,34 @@
-console.log("i am conntected")
+console.log("I am connected");
+
+const userDiv = document.getElementById("div");
+console.log(userDiv);
+
+fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(data => {
+
+        userDiv.innerHTML = `
+            <table class="user-table">
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>City</th>
+                        <th>Email</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    ${data.map(user => `
+                        <tr>
+                            <td>${user.name}</td>
+                            <td>${user.address.city}</td>
+                            <td>${user.email}</td>
+                        </tr>
+                    `).join("")}
+                </tbody>
+            </table>
+        `;
+
+    })
+    .catch(error => {
+        console.log("Error:", error);
+    });
